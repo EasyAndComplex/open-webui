4cdc61c00bf6bd6b272116f638c64f70b425f953
Steven <bobtwim@hotmail.com>
2025-02-20 13:58:14 +0100

    change the name WebUI to AI

--- INSTALLATION.md
+++ INSTALLATION.md
@@ -1,4 +1,4 @@
-### Installing Both Ollama and FiatLux WebUI Using Kustomize
+### Installing Both Ollama and FiatLux AI Using Kustomize
 
 For cpu-only pod
 
@@ -12,7 +12,7 @@ For gpu-enabled pod
 kubectl apply -k ./kubernetes/manifest
 ```
 
-### Installing Both Ollama and FiatLux WebUI Using Helm
+### Installing Both Ollama and FiatLux AI Using Helm
 
 Package Helm file first
 
--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -106,9 +106,9 @@ for source in log_sources:
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "FiatLux WebUI")
-if WEBUI_NAME != "FiatLux WebUI":
-    WEBUI_NAME += " (FiatLux WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "FiatLux AI")
+if WEBUI_NAME != "FiatLux AI":
+    WEBUI_NAME += " (FiatLux AI)"
 
 WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
 
